5d0b17f751bf36852013e56a62a9443e2a278c22
Pierre van Male <pierre-van-male_student2021@wilder.school>
2021-10-25 16:01:42 +0200

    fetching simpson quote upon request

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios'
+import { useState } from 'react'
+import './App.css'
 
 function App() {
+  const [quote, setQuote] = useState(
+    {
+      "quote": "My eyes! The goggles do nothing!",
+      "character": "Rainier Wolfcastle",
+      "image": "https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FRainierWolfcastle.png?1497567511035",
+      "characterDirection": "Right"
+    }
+  )
+
+  const getQuote = () => {
+    // Send the request
+    axios
+      .get('https://simpsons-quotes-api.herokuapp.com/quotes')
+      // Extract the DATA from the received response
+      .then((response) => response.data)
+      // Use this data to update the state
+      .then((data) => setQuote(data[0]))
+      .then(console.log("quote: ", quote))
+  
+
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button type="button" onClick={getQuote}>Get quote</button>
+      <div className='DisplayQuote'>
+        <img src={quote.image} alt={quote.character} />
+        <p>{quote.character}</p>
+        <p>{quote.quote}</p>
+      </div>
+ 
     </div>
   );
 }
